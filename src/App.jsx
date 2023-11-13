614256d4a17e303960262b8b932c6efa1f98371c
G-U-T <147336533+G-U-T@users.noreply.github.com>
2023-11-13 15:07:04 -0500

    Renders puppies

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import './App.css'
+import {useState} from 'react'
+import {puppyList} from './data.js'
 
 function App() {
-  return (
-    <>
-      <div>
-      </div>
-    </>
-  )
+	const [puppies, setPuppies] = useState(puppyList);
+
+	// console.log(puppies);
+
+	return (
+		<div>
+			{
+				puppies.map((puppy) => {
+					return <p key={puppy.id}>{puppy.name}</p>
+				})
+			}
+		</div>
+	)
 }
 
 export default App
