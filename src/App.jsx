c2736ed4904bdab02e60f11c1f935f9f3a0d220d
G-U-T <147336533+G-U-T@users.noreply.github.com>
2023-11-13 15:26:47 -0500

    Features puppies properly.

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,24 @@ import {puppyList} from './data.js'
 
 function App() {
 	const [puppies, setPuppies] = useState(puppyList);
+	const [featuredPupID, setFeaturedPupID] = useState(null);
 
-	// console.log(puppies);
+	const featuredPup = puppies.find((pup) => pup.id === featuredPupID);
 
 	return (
 		<div>
+			{featuredPupID && (
+				<>
+					<h2>{featuredPup.name}</h2>
+					<ul>
+						<li>Age: {featuredPup.age}</li>
+						<li>Email: {featuredPup.email}</li>
+					</ul>
+				</>
+			)}
 			{
 				puppies.map((puppy) => {
-					return <p key={puppy.id}>{puppy.name}</p>
+					return <p key={puppy.id} onClick={()=>{setFeaturedPupID(puppy.id)}}>{puppy.name}</p>
 				})
 			}
 		</div>
